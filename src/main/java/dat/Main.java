2d412dbcb5e6eb8ca2466462d7185a1ffe83404d
Jeanette93Cph <jeanette.93@live.dk>
2024-09-18 09:33:03 +0200

    Added getAllActorsJSON
    Added ActorService with methods. Added DirectorService, GenreService without methods.

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -1,6 +1,7 @@
 package dat;
 
 import dat.dtos.MovieDTO;
+import dat.services.ActorService;
 import dat.services.MovieService;
 import java.util.List;
 
@@ -8,13 +9,15 @@ public class Main
 {
     public static void main(String[] args)
     {
+        //Printing all danish movies for the recent 5 years in JSON
+        String jsonAllMovies = MovieService.getAllMoviesJSON(2);
+       // System.out.println(jsonAllMovies);
 
-        String jsonAllMovies = MovieService.getAllMoviesJSON();
-        //System.out.println(jsonAllMovies);
-
+        //Printing all danish movies for the recent 5 years as MovieDTO's
         List<MovieDTO> moviesDTOs = MovieDTO.convertToDTOFromJSONList(jsonAllMovies);
-         moviesDTOs.forEach(System.out::println);
+        //moviesDTOs.forEach(System.out::println);
 
-
+        //Printing all cast from the danish movies for the recent 5 years
+        ActorService.getAllActorsJSON(2).forEach(System.out::println);
     }
 }
--- src/main/java/dat/services/MovieService.java
+++ src/main/java/dat/services/MovieService.java
@@ -1,5 +1,11 @@
 package dat.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dat.dtos.MovieDTO;
+import dat.dtos.MovieResponseDTO;
+
 import javax.imageio.IIOException;
 import java.io.IOException;
 import java.net.URI;
@@ -7,6 +13,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MovieService
 {
@@ -15,10 +22,10 @@ public class MovieService
     private static final String API_KEY = System.getenv("api_key");
     private static final String BASE_URL_MOVIE_DANISH_RECENT_5_YEARS = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_date.gte=2019-01-01&primary_release_date.lte=2024-10-01&sort_by=popularity.desc&with_original_language=da";
 
-    public static String getAllMoviesJSON()
+    public static String getAllMoviesJSON(int page)
     {
 
-        String url = BASE_URL_MOVIE_DANISH_RECENT_5_YEARS + "&api_key=" + API_KEY;
+        String url = BASE_URL_MOVIE_DANISH_RECENT_5_YEARS + "&api_key=" + API_KEY + "&page=" + page;
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest
@@ -40,6 +47,22 @@ public class MovieService
 
     }
 
+
+    public static List<Long> getAllMoviesIDJSON(String json)
+    {
+        try
+        {
+            ObjectMapper objectMapper = new ObjectMapper();
+            MovieResponseDTO movieResponseDTO = objectMapper.readValue(json, MovieResponseDTO.class);
+            List<MovieDTO> movieDTOS = movieResponseDTO.getMovieList();
+            return movieDTOS.stream().map(MovieDTO::getId).collect(Collectors.toList());
+
+        } catch (JsonProcessingException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 
 }
