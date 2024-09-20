3cc671bfcea71d1a0ba5efad526f9b537bd7ebb8
Felicia Favrholdt <fillefilejs@Felicias-MacBook-Air.local>
2024-09-20 10:52:09 +0200

    updates class diagram

--- src/main/java/dat/services/ActorService.java
+++ src/main/java/dat/services/ActorService.java
@@ -18,8 +18,7 @@ public class ActorService {
 	private static final String API_KEY = System.getenv("api_key");
 	private static final String BASE_URL_MOVIE_DANISH_RECENT_5_YEARS = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_date.gte=2019-01-01&primary_release_date.lte=2024-10-01&sort_by=popularity.desc&with_original_language=da";
 	private static final String URL = "https://api.themoviedb.org/3/movie/";
-
-	private static HttpClient client = HttpClient.newHttpClient();
+	private final static HttpClient client = HttpClient.newHttpClient();
 
 
 	//with help from chatgpt
