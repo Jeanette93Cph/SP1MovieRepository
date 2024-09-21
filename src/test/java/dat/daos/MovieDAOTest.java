c9f89f69f42bfbddcf597ced7f9293296c374c37
Felicia Favrholdt <fillefilejs@Felicias-MacBook-Air.local>
2024-09-21 12:13:01 +0200

    create test class - need implementation

---
+++ src/test/java/dat/daos/MovieDAOTest.java
@@ -0,0 +1,39 @@
+package dat.daos;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MovieDAOTest {
+
+	@Test
+	void saveMovies () {
+	}
+
+	@Test
+	void create () {
+	}
+
+	@Test
+	void read () {
+	}
+
+	@Test
+	void update () {
+	}
+
+	@Test
+	void delete () {
+	}
+
+	@Test
+	void findById () {
+	}
+
+	@Test
+	void findAll () {
+	}
+
+	@Test
+	void searchForMovieByGenre () {
+	}
+}
