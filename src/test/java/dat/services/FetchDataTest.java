271323f92abc9552ad1cb4ce373af848499f4399
Felicia Favrholdt <fillefilejs@Felicias-MacBook-Air.local>
2024-09-22 19:37:31 +0200

    adds test setup for FetchData

---
+++ src/test/java/dat/services/FetchDataTest.java
@@ -0,0 +1,86 @@
+package dat.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FetchDataTest {
+
+	@BeforeEach
+	void setUp () {
+	}
+
+	@AfterEach
+	void tearDown () {
+	}
+
+	@Test
+	void getAllMovies () {
+	}
+
+	@Test
+	void getAllDirectors () {
+	}
+
+	@Test
+	void getAllActors () {
+	}
+
+	@Test
+	void getAllGenre () {
+	}
+
+	@Test
+	void persistEntity () {
+	}
+
+	@Test
+	void findEntity () {
+	}
+
+	@Test
+	void updateEntity () {
+	}
+
+	@Test
+	void removeEntity () {
+	}
+
+	@Test
+	void findDirectorInASpecificMovie () {
+	}
+
+	@Test
+	void findActorsInASpecificMovie () {
+	}
+
+	@Test
+	void findGenreInASpecificMovie () {
+	}
+
+	@Test
+	void findAllMoviesInASpecificGenre () {
+	}
+
+	@Test
+	void getMoviesByTitle () {
+	}
+
+	@Test
+	void getAverageRatingOfAllMovies () {
+	}
+
+	@Test
+	void getTop10LowestRatedMovies () {
+	}
+
+	@Test
+	void getTop10HighestRatedMovies () {
+	}
+
+	@Test
+	void getTop10MostPopularMovies () {
+	}
+}
